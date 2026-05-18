89aa0dfc277f819760ad1beb531e79a62b768ea6
Stevo John <stevehuwjohn@gmail.com>
2026-05-18 13:21:45 +0100

    Work...

--- src/RubiksCube.Core/Solver.cs
+++ src/RubiksCube.Core/Solver.cs
@@ -1,10 +1,16 @@
-﻿using RubiksCube.Core.Models;
+﻿using System.Diagnostics;
+using RubiksCube.Core.Models;
 
 namespace RubiksCube.Core;
 
 public class Solver
 {
-    public List<Move> Solve(Cube cube)
+    public (bool Solved, List<Move> Moves, TimeSpan Duration) Solve(Cube cube)
     {
+        var stopwatch = Stopwatch.StartNew();
+        
+        stopwatch.Stop();
+        
+        return (false, null, stopwatch.Elapsed);
     }
 }
