fde5d72ec0681d857c5da89f9778ffae099b3a5f
Stevo John <stevehuwjohn@gmail.com>
2026-05-18 21:56:19 +0100

    Work

--- src/RubiksCube.Core/Solver.cs
+++ src/RubiksCube.Core/Solver.cs
@@ -6,23 +6,23 @@ namespace RubiksCube.Core;
 public class Solver
 {
     private readonly Cube _cube;
-    
+
     private static readonly Move[] AllMoves;
 
     static Solver()
     {
         var faces = Enum.GetValues<Face>();
-        
+
         AllMoves = new Move[faces.Length * 3];
 
         var index = 0;
-        
+
         foreach (var face in faces)
         {
             AllMoves[index++] = new Move(face, Direction.Clockwise);
-            
+
             AllMoves[index++] = new Move(face, Direction.AntiClockwise);
-            
+
             AllMoves[index++] = new Move(face, Direction.HalfTurn);
         }
     }
@@ -42,6 +42,8 @@ public class Solver
 
         var stopwatch = Stopwatch.StartNew();
 
+        FindWhiteCross();
+        
         stopwatch.Stop();
 
         return (false, _moves, stopwatch.Elapsed);
@@ -49,5 +51,70 @@ public class Solver
 
     private void FindWhiteCross()
     {
+        for (var maxDepth = 0; maxDepth <= 8; maxDepth++)
+        {
+            if (SearchWhiteCross(maxDepth, null))
+            {
+                return;
+            }
+        }
+    }
+
+    private bool SearchWhiteCross(int depth, Face? previousFace)
+    {
+        if (HasWhiteCross())
+        {
+            return true;
+        }
+
+        if (depth == 0)
+        {
+            return false;
+        }
+
+        foreach (var move in AllMoves)
+        {
+            if (move.Face == previousFace)
+            {
+                continue;
+            }
+
+            _cube.ApplyMove(move.Face, move.Direction);
+            _moves.Add(move);
+
+            if (SearchWhiteCross(depth - 1, move.Face))
+            {
+                return true;
+            }
+
+            _moves.RemoveAt(_moves.Count - 1);
+            _cube.ApplyMove(move.Face, Opposite(move.Direction));
+        }
+
+        return false;
+    }
+
+    private static Direction Opposite(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Clockwise => Direction.AntiClockwise,
+            Direction.AntiClockwise => Direction.Clockwise,
+            Direction.HalfTurn => Direction.HalfTurn,
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+    }
+
+    private bool HasWhiteCross()
+    {
+        return
+            _cube[Face.Up, 1, 0] == Colour.White &&
+            _cube[Face.Up, 0, 1] == Colour.White &&
+            _cube[Face.Up, 2, 1] == Colour.White &&
+            _cube[Face.Up, 1, 2] == Colour.White &&
+            _cube[Face.Back, 1, 0] == _cube[Face.Back, 1, 1] &&
+            _cube[Face.Left, 1, 0] == _cube[Face.Left, 1, 1] &&
+            _cube[Face.Right, 1, 0] == _cube[Face.Right, 1, 1] &&
+            _cube[Face.Front, 1, 0] == _cube[Face.Front, 1, 1];
     }
 }
