dc54104a6c90050d8f0140f4330133e21bad1c73
Stevo John <stevehuwjohn@gmail.com>
2026-05-18 12:07:57 +0100

    Work...

--- src/RubiksCube.Core/Models/Cube.cs
+++ src/RubiksCube.Core/Models/Cube.cs
@@ -60,15 +60,15 @@ public class Cube
 
         if (direction == Direction.Clockwise)
         {
-            (matrix[0, 0], matrix[2, 0], matrix[2, 2], matrix[0, 2]) = (matrix[2, 0], matrix[2, 2], matrix[0, 2], matrix[0, 0]);
-
-            (matrix[1, 0], matrix[2, 1], matrix[1, 2], matrix[0, 1]) = (matrix[2, 1], matrix[1, 2], matrix[0, 1], matrix[1, 0]);
-        }
-        else
-        {
             (matrix[0, 0], matrix[2, 0], matrix[2, 2], matrix[0, 2]) = (matrix[0, 2], matrix[0, 0], matrix[2, 0], matrix[2, 2]);
 
             (matrix[1, 0], matrix[2, 1], matrix[1, 2], matrix[0, 1]) = (matrix[0, 1], matrix[1, 0], matrix[2, 1], matrix[1, 2]);
+        }
+        else
+        {
+            (matrix[0, 0], matrix[2, 0], matrix[2, 2], matrix[0, 2]) = (matrix[2, 0], matrix[2, 2], matrix[0, 2], matrix[0, 0]);
+
+            (matrix[1, 0], matrix[2, 1], matrix[1, 2], matrix[0, 1]) = (matrix[2, 1], matrix[1, 2], matrix[0, 1], matrix[1, 0]);
         }
     }
 
