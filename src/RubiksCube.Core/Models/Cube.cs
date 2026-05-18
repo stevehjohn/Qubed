479e496a2fd54482b79bc50fc3ba40c494e2a94b
Stevo John <stevehuwjohn@gmail.com>
2026-05-18 15:09:59 +0100

    Working...

--- src/RubiksCube.Core/Models/Cube.cs
+++ src/RubiksCube.Core/Models/Cube.cs
@@ -157,7 +157,7 @@ public class Cube
 
                 break;
 
-            default:
+            case Direction.HalfTurn:
                 (matrix[0, 0], matrix[2, 2], matrix[2, 0], matrix[0, 2]) = (matrix[2, 2], matrix[0, 0], matrix[0, 2], matrix[2, 0]);
 
                 (matrix[1, 0], matrix[1, 2], matrix[0, 1], matrix[2, 1]) = (matrix[1, 2], matrix[1, 0], matrix[2, 1], matrix[0, 1]);
@@ -201,7 +201,7 @@ public class Cube
 
                 break;
 
-            default:
+            case Direction.HalfTurn:
                 WriteSlice(slices[0], values[2]);
                 
                 WriteSlice(slices[1], values[3]);
