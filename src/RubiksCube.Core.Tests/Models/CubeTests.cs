8ffecb26d46228fe587ddfd1b1d2227803203207
Stevo John <stevehuwjohn@gmail.com>
2026-05-18 12:59:41 +0100

    Work...

--- src/RubiksCube.Core.Tests/Models/CubeTests.cs
+++ src/RubiksCube.Core.Tests/Models/CubeTests.cs
@@ -1,6 +1,59 @@
+using RubiksCube.Core.Models;
+using Xunit;
+
 namespace RubiksCube.Core.Tests.Models;
 
 public class CubeTests
 {
+    [Theory]
+    [InlineData(Face.Up)]
+    [InlineData(Face.Down)]
+    [InlineData(Face.Front)]
+    [InlineData(Face.Back)]
+    [InlineData(Face.Left)]
+    [InlineData(Face.Right)]
+    public void FourClockwiseTurns_ReturnsToSolved(Face face)
+    {
+        var cube = new Cube();
+        var expected = new Cube();
+
+        for (var i = 0; i < 4; i++)
+        {
+            cube.ApplyMove(face, Direction.Clockwise);
+        }
+
+        AssertCubeEqual(expected, cube);
+    }
     
+    [Theory]
+    [InlineData(Face.Up)]
+    [InlineData(Face.Down)]
+    [InlineData(Face.Front)]
+    [InlineData(Face.Back)]
+    [InlineData(Face.Left)]
+    [InlineData(Face.Right)]
+    public void ClockwiseThenAntiClockwise_ReturnsToSolved(Face face)
+    {
+        var cube = new Cube();
+        var expected = new Cube();
+
+        cube.ApplyMove(face, Direction.Clockwise);
+        cube.ApplyMove(face, Direction.AntiClockwise);
+
+        AssertCubeEqual(expected, cube);
+    }
+    
+    private static void AssertCubeEqual(Cube expected, Cube actual)
+    {
+        foreach (var face in Enum.GetValues<Face>())
+        {
+            for (var x = 0; x < 3; x++)
+            {
+                for (var y = 0; y < 3; y++)
+                {
+                    Assert.Equal(expected[face, x, y], actual[face, x, y]);
+                }
+            }
+        }
+    }
 }
