9253ca8d8e085c49b9d3fd6dd59b3933685a713c
Stevo John <stevehuwjohn@gmail.com>
2026-05-19 22:42:52 +0100

    Work...:

--- src/RubiksCube.Core/Solver.cs
+++ src/RubiksCube.Core/Solver.cs
@@ -94,8 +94,14 @@ public class Solver
 
         Console.WriteLine("\nRemaining Corners\n");
 
-        Console.WriteLine(BruteForce(HasAlignedYellowCorners, Face.Up));
+        Console.WriteLine(BruteForce(HasGryCorner));
 
+        Console.WriteLine(BruteForce(HasRbyCorner));
+
+        Console.WriteLine(BruteForce(HasGoyCorner));
+
+        Console.WriteLine(BruteForce(HasBoyCorner));
+        
         Console.WriteLine(_cube.ToString());
 
         stopwatch.Stop();
@@ -339,18 +345,33 @@ public class Solver
                && cube[Face.Left, 1, 2] == Colour.Green;
     }
 
-    private static bool HasAlignedYellowCorners(Cube cube)
+    private static bool HasGryCorner(Cube cube)
     {
         return HasAlignedYellowCross(cube)
                && cube[Face.Left, 2, 2] == Colour.Green
                && cube[Face.Front, 0, 2] == Colour.Red
-               && cube[Face.Down, 0, 0] == Colour.Yellow
+               && cube[Face.Down, 0, 0] == Colour.Yellow;
+    }
+
+    private static bool HasRbyCorner(Cube cube)
+    {
+        return HasGryCorner(cube)
                && cube[Face.Front, 2, 2] == Colour.Red
                && cube[Face.Right, 0, 2] == Colour.Blue
-               && cube[Face.Down, 2, 0] == Colour.Yellow
+               && cube[Face.Down, 2, 0] == Colour.Yellow;
+    }
+
+    private static bool HasGoyCorner(Cube cube)
+    {
+        return HasRbyCorner(cube)
                && cube[Face.Left, 0, 2] == Colour.Green
                && cube[Face.Back, 2, 2] == Colour.Orange
-               && cube[Face.Down, 0, 2] == Colour.Yellow
+               && cube[Face.Down, 0, 2] == Colour.Yellow;
+    }
+
+    private static bool HasBoyCorner(Cube cube)
+    {
+        return HasGoyCorner(cube)
                && cube[Face.Back, 0, 2] == Colour.Orange
                && cube[Face.Right, 2, 2] == Colour.Blue
                && cube[Face.Down, 2, 2] == Colour.Yellow;
