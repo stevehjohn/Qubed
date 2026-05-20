196584084fbc2d65586007941671a40bc2d8a4b3
Stevo John <stevehuwjohn@gmail.com>
2026-05-20 10:51:31 +0100

    Working

---
+++ src/RubiksCube.Core/Logic/AlgorithmLibrary.cs
@@ -0,0 +1,102 @@
+using RubiksCube.Core.Models;
+
+namespace RubiksCube.Core.Logic;
+
+public class AlgorithmLibrary
+{
+    private static readonly List<List<Move>> LayerTwoMovesInternal = [];
+
+    private static readonly string[] LayerTwoAlgorithms =
+    [
+        "D R D' R' D' F' D F",
+        "D' L' D L D F D' F'",
+        
+        "D B D' B' D' R' D R",
+        "D' L' D L D R D' R'",
+        
+        "D L D' L' D' B' D B",
+        "D' L' D L D B D' B'",
+
+        "D F D' F' D' L' D L",
+        "D' L' D L D L D' L'"
+    ];
+
+    public static IReadOnlyList<IReadOnlyList<Move>> LayerTwoMoves => LayerTwoMovesInternal;
+
+    static AlgorithmLibrary()
+    {
+        foreach (var algorithm in LayerTwoAlgorithms)
+        {
+            LayerTwoMovesInternal.Add(ParseAlgorithm(algorithm));
+        }
+    }
+
+    private static List<Move> ParseAlgorithm(string algorithm)
+    {
+        var steps = algorithm.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        var moves = new List<Move>();
+
+        foreach (var step in steps)
+        {
+            moves.Add(ParseMove(step));
+        }
+
+        return moves;
+    }
+
+    private static Move ParseMove(string move)
+    {
+        Face face;
+        
+        switch (move[0])
+        {
+            case 'U':
+                face = Face.Up;
+                
+                break;
+
+            case 'D':
+                face = Face.Down;
+                
+                break;
+            
+            case 'L':
+                face = Face.Left;
+                
+                break;
+            
+            case 'R':
+                face = Face.Right;
+                
+                break;
+            
+            case 'F':
+                face = Face.Front;
+                
+                break;
+            
+            case 'B':
+                face = Face.Back;
+                
+                break;
+            
+            default:
+                throw new ParseException($"Unknown move {move[0]}");
+        }
+
+        var direction = Direction.Clockwise;
+
+        if (move.Length > 0)
+        {
+            direction = move[1] switch
+            {
+                '\'' => Direction.AntiClockwise,
+                '2' => Direction.HalfTurn,
+                _ => throw new ParseException($"Unknown modifier {move[1]}")
+            };
+        }
+
+        return new Move(face, direction);
+    }
+}
---
+++ src/RubiksCube.Core/ParseException.cs
@@ -0,0 +1,6 @@
+namespace RubiksCube.Core;
+
+public class ParseException : Exception
+{
+    public ParseException(string message) : base(message) { }
+}
