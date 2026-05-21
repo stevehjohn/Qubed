13fb135eee24850a64ab348a71541bfffbec636f
Stevo John <stevehuwjohn@gmail.com>
2026-05-21 19:31:14 +0100

    Working...

--- src/RubiksCube.Core/Logic/AlgorithmLibrary.cs
+++ src/RubiksCube.Core/Logic/AlgorithmLibrary.cs
@@ -94,7 +94,8 @@ public abstract class AlgorithmLibrary
         (
             "Step 4 - Yellow Cross",
             [
-                "R' D' F' D F R"
+                "F U R U' R' F'",
+                "F R U R' U' F'"
             ],
             [
                 cube => cube[Face.Down, 1, 0] == Colour.Yellow
