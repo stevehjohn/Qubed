aa8b2c0eac9c6729156af491c8b10ea4abf51d47
Stevo John <stevehuwjohn@gmail.com>
2026-05-22 19:39:20 +0100

    Better

--- src/RubiksCube.Core/Solver.cs
+++ src/RubiksCube.Core/Solver.cs
@@ -199,7 +199,7 @@ public class Solver
                 }
             }
 
-            if (occurrences > 0)
+            if (occurrences > 1)
             {
                 continue;
             }
