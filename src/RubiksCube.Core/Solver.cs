9161fb8288b42595fcdb1366a81429e8d053e3d6
Stevo John <stevehuwjohn@gmail.com>
2026-05-22 12:29:11 +0100

    Working again.

--- src/RubiksCube.Core/Solver.cs
+++ src/RubiksCube.Core/Solver.cs
@@ -9,7 +9,7 @@ public class Solver
 {
     private const int MinDepth = 1;
 
-    private const int MaxDepth = 15;
+    private const int MaxDepth = 20;
 
     private readonly Cube _cube;
 
@@ -154,8 +154,8 @@ public class Solver
         {
             return false;
         }
-        
-        _visitedDepths[key] = depth;
+
+        _visitedDepths.AddOrUpdate(key, depth, (_, oldDepth) => Math.Max(oldDepth, depth));
         
         foreach (var set in moveSet)
         {
