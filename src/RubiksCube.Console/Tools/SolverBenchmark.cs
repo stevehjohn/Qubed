9006f68c8376a93273c2826c68d9196564ed51f8
Stevo John <stevehuwjohn@gmail.com>
2026-05-24 17:18:55 +0100

    Noice

--- src/RubiksCube.Console/Tools/SolverBenchmark.cs
+++ src/RubiksCube.Console/Tools/SolverBenchmark.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using RubiksCube.Core;
 using RubiksCube.Core.Models;
 using static System.Console;
@@ -11,6 +12,10 @@ public static class SolverBenchmark
         var totalMoves = 0;
 
         var totalDuration = TimeSpan.Zero;
+
+        var statistics = new List<(int Moves, TimeSpan Duration)>();
+
+        var stopwatch = Stopwatch.StartNew();
         
         for (var i = 1; i <= iterations; i++)
         {
@@ -20,21 +25,37 @@ public static class SolverBenchmark
             
             var solver = new Solver(cube);
 
-            WriteLine($"\nIteration {i}/{iterations}.");
-            
-            WriteLine();
+            WriteLine($"\nIteration {i}/{iterations}.\n");
             
             WriteLine(cube.ToString());
             
             var result = solver.Solve();
-            
+
+            foreach (var move in result.Moves)
+            {
+                cube.ApplyMove(move);
+            }
+
             WriteLine(cube.ToString());
             
             totalMoves += result.Moves.Count;
 
             totalDuration += result.Duration;
             
-            WriteLine($"Moves: {result.Moves.Count}, duration: {result.Duration:ss\\.fff}s. Average moves: {(double) totalMoves / i:N2}, average duration {totalDuration / i:ss\\.fff}.\n");
+            statistics.Add((result.Moves.Count, result.Duration));
+            
+            WriteLine(@$"Moves: {result.Moves.Count}, duration: {result.Duration:ss\.fff}s. Average moves: {(double) totalMoves / i:N2}, average duration {totalDuration / i:ss\.fff}.\n");
         }
+        
+        stopwatch.Stop();
+        
+        WriteLine("\nSummary:\n");
+
+        foreach (var statistic in statistics)
+        {
+            WriteLine(@$"Moves: {statistic.Moves}, duration: {statistic.Duration:ss\.fff}s.");
+        }
+        
+        WriteLine($@"\nTotal duration: {totalDuration:mm\.ss\.fff}.\n");
     }
 }
