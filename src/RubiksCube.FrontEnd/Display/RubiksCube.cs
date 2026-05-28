474f0defd8ffab3abb421ab6775f66599b2a63ba
Stevo John <stevehuwjohn@gmail.com>
2026-05-28 17:19:35 +0100

    Code clean

--- src/RubiksCube.FrontEnd/Display/RubiksCube.cs
+++ src/RubiksCube.FrontEnd/Display/RubiksCube.cs
@@ -660,6 +660,7 @@ public sealed class RubiksCube : Game
         do
         {
             face = (Face) _random.Next(6);
+            
         } while (face == _previousFace);
         
         StartFaceRotation(new Move(face, (Direction) _random.Next(3)));
