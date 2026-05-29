79ac7ba200e5b893300c8ae830a03cee2807f50a
Stevo John <stevehuwjohn@gmail.com>
2026-05-29 13:48:12 +0100

    Removed

---
+++ clean.sh
@@ -0,0 +1,9 @@
+find . -name "*.orig" -type f -delete
+rm -rf ./src/Qubed.Console/bin
+rm -rf ./src/Qubed.Console/obj
+rm -rf ./src/Qubed.Core/bin
+rm -rf ./src/Qubed.Core/obj
+rm -rf ./src/Qubed.Core.Tests/bin
+rm -rf ./src/Qubed.Core.Tests/obj
+rm -rf ./src/Qubed.FrontEnd/bin
+rm -rf ./src/Qubed.FrontEnd/obj
