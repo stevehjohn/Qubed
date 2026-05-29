5ba1bd9151905cf5ea2aed2fcce84088debe8d52
Stevo John <stevehuwjohn@gmail.com>
2026-05-29 14:41:14 +0100

    Text

--- src/Qubed.FrontEnd/Display/TextManager.cs
+++ src/Qubed.FrontEnd/Display/TextManager.cs
@@ -6,21 +6,26 @@ namespace Qubed.FrontEnd.Display;
 public class TextManager
 {
     private readonly SpriteBatch _spriteBatch;
-    
+
     private readonly SpriteFont _font;
 
     public TextManager(SpriteBatch spriteBatch, SpriteFont font)
     {
         _spriteBatch = spriteBatch;
-        
+
         _font = font;
     }
 
-    public void DrawMessage(string message, int left, int top)
+    public void DrawMessage(string message, int left, int top, bool center = false)
     {
         if (message == null)
         {
             return;
+        }
+
+        if (center)
+        {
+            left -= (int) _font.MeasureString(message).X;
         }
 
         for (var y = -2; y < 3; y++)
@@ -31,6 +36,6 @@ public class TextManager
             }
         }
 
-        _spriteBatch.DrawString(_font, message, new Vector2(left, top), Color.FromNonPremultiplied(255, 255, 255, 255), 0, Vector2.Zero, Vector2.One, SpriteEffects.None, .7f);    
+        _spriteBatch.DrawString(_font, message, new Vector2(left, top), Color.FromNonPremultiplied(255, 255, 255, 255), 0, Vector2.Zero, Vector2.One, SpriteEffects.None, .7f);
     }
 }
